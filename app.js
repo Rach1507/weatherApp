b93b976802fb17ad0f3a35e761ec8ad90064db8e
RachithaBR <59915937+Rach1507@users.noreply.github.com>
2021-12-05 20:27:04 +0530

    Update app.js
    Send response (Temperature and weather description & image icon representing the weather).

--- app.js
+++ app.js
@@ -6,26 +6,44 @@ const app = express();
 app.get('/', function(req, res) {
 
   const url = "https://api.openweathermap.org/data/2.5/weather?appid=5aebcfcc860581a4b109cef98866a494&q=London,uk&units=metric";
+  
+  // function(res) is called when the connection is established
+
+  // on('data') is called when there's a chunk of data (this almost certainly will be more than once)
+
+  // on('close') is called when the connection closes.
+
+
   https.get(url, function(response) {
 
+    response.on("data", function(data) {
 
+      const weatherData = JSON.parse(data)
 
-        response.on("data", function(data) {
-
-        const weatherData = JSON.parse(data)
-
-
-        const weatherDes = weatherData.weather[0].description;
-        console.log(weatherDes);
-
-
+      const temp = weatherData.main.temp;
+      const weatherDes = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL ="http://openweathermap.org/img/wn//"+ icon +"@2x.png";
 
 
 
 
+      // const weatherStr = "The weather at London is " + weatherDes;
+      // res.send(" <p><h1> " + weatherStr + " The temp is "+ temp + "</h1></p>");
 
-        const weatherStr = "The weather at London is " + weatherDes;
-        res.send(weatherStr);
+
+      //
+      // const weatherStr = "The weather at London is " + weatherDes;
+      // res.send("  <h1> " + weatherStr + "</h1>");
+      
+
+      const weatherStr = "The weather at London is " + weatherDes;
+      res.write("  <h1> " + weatherStr + ".</h1>");
+      res.write(" <h3> " +  " The temp is " + temp + " degree celcsius .</h3>");
+      res.write("<img src= " + imageURL + " alt= 'weather-icon'>");
+
+      res.send();
+
     })
   });
 
